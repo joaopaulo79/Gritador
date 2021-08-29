9dafe9723e11415eeedb45fcd1fab48f66103eb4
João Paulo <joaopdejesus79@gmail.com>
2021-08-28 21:41:19 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Berro;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("Você diz: ");
+            Console.ResetColor();
+            Berro = Console.ReadLine();
+            Berro = Berro.ToUpper();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("Eu grito: ");
+            Console.ResetColor();
+            Console.WriteLine(Berro);
+            Console.WriteLine();
+        }
+    }
+}
